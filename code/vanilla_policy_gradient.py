9a995d6a1bf5b88a936f8917d2742e030504e9a0
Jason Wei <jason.weng.wei@gmail.com>
2023-01-08 16:31:04 -0800

    Update vanilla_policy_gradient.py

--- code/vanilla_policy_gradient.py
+++ code/vanilla_policy_gradient.py
@@ -1,5 +1,4 @@
 # From http://rl-gym-doc.s3-website-us-west-2.amazonaws.com/mlss/pg-startercode.py
-
 import gymnasium as gym
 import numpy as np
 import torch
@@ -7,27 +6,26 @@ device = torch.device("cpu")
 torch.manual_seed(0)
 
 
-def discount(x, gamma):
+def discount(rewards, gamma):
     """
-    Given vector x, computes a vector y such that
-    y[i] = x[i] + gamma * x[i+1] + gamma^2 x[i+2] + ...
+    Given vector rewards, computes a vector y such that
+    discounted_reward_to_go[i] = rewards[i] + gamma * rewards[i+1] + gamma^2 * rewards[i+2] + ...
+    Example: discount(np.array([1, 1, 1]), gamma=0.9) returns [2.71 1.9  1.  ]
     """
-    out = np.zeros(len(x), 'float64')
-    out[-1] = x[-1]
-    for i in reversed(range(len(x)-1)):
-        out[i] = x[i] + gamma*out[i+1]
-    assert x.ndim >= 1
-    # More efficient version:
-    # scipy.signal.lfilter([1],[1,-gamma],x[::-1], axis=0)[::-1]
-    return out
+    discounted_reward_to_go = np.zeros(len(rewards), 'float64')
+    discounted_reward_to_go[-1] = rewards[-1]
+    for i in reversed(range(len(rewards)-1)):
+        discounted_reward_to_go[i] = rewards[i] + gamma * discounted_reward_to_go[i+1]
+    assert rewards.ndim >= 1
+    return discounted_reward_to_go
 
 
 def categorical_sample(prob_n):
     """
-    Sample from categorical distribution,
-    specified by a vector of class probabilities
+    Sample from categorical distribution, specified by a vector of class probabilities.
     """
-    prob_n = prob_n.detach().numpy()
+    if type(prob_n) == torch.Tensor:
+        prob_n = prob_n.detach().numpy()
     csprob_n = np.cumsum(prob_n)
     return (csprob_n > np.random.rand()).argmax()
 
@@ -59,17 +57,23 @@ def get_trajectory(agent, environment, episode_max_length=1, render=False):
 
 
 class REINFORCEAgent(torch.nn.Module):
+
     def __init__(self, environment, **userconfig):
+
         super().__init__()
         num_inputs = environment.observation_space.shape[0]
         num_outputs = environment.action_space.n
 
-        # Set algorithm parameters
-        self.config = dict(episode_max_length=100, timesteps_per_batch=10000, n_iter=100, 
-            gamma=1.0, stepsize=0.05, hidden_units=20)
+        self.config = dict(
+            episode_max_length=100, 
+            timesteps_per_batch=10000, 
+            num_iterations=100, 
+            gamma=1.0, 
+            stepsize=0.05, 
+            hidden_units=20
+        )
         self.config.update(userconfig)
 
-        # Define neural network with one hidden layer
         self.fc1 = torch.nn.Linear(num_inputs, self.config['hidden_units'])
         self.fc2 = torch.nn.Linear(self.config['hidden_units'], num_outputs)
 
@@ -77,36 +81,51 @@ class REINFORCEAgent(torch.nn.Module):
         x = torch.tanh(self.fc1(observations))
         x = self.fc2(x)
         return torch.softmax(x, dim=1)
-    
-    def compute_prob(self, observations):
-        return self.forward(observations)
-
-    def policy_gradient_update(self, observations, actions, advantages, stepsize):
-        predicted_softmax_probabilities = self.forward(observations)
-        num_examples = observations.shape[0]
-        predicted_action_probabilities = predicted_softmax_probabilities[torch.arange(num_examples), actions]
-        loss = torch.log(predicted_action_probabilities).dot(advantages.type(predicted_action_probabilities.type())) / num_examples
-        # print(f'Advantage sum: {torch.sum(advantages)}')
-        print(f'loss: \t {loss}')
-        loss.backward()
-        with torch.no_grad():
-            for param in self.parameters():
-                param.data += stepsize * param.grad  # +=?
-        self.zero_grad()
 
     def act(self, ob):
         if type(ob) == tuple:
             torch_input = torch.from_numpy(ob[0].reshape(1,-1))
         else:
             torch_input = torch.from_numpy(ob.reshape(1,-1))
-        prob = self.compute_prob(torch_input)
+        prob = self.forward(torch_input)
         action = categorical_sample(prob)
         return action
+
+    def policy_gradient_update(self, observations, actions, advantages, stepsize):
+
+        observations = torch.from_numpy(observations)
+        actions = torch.from_numpy(actions)
+        advantages = torch.from_numpy(advantages)
+        num_examples = observations.shape[0]
+
+        # Forward pass. 
+        # Input is observations, shape [timesteps_per_batch, observation space].
+        # Output is predicted_softmax_probabilities, shape [timesteps_per_batch, action space].
+        # E.g., for Cartpole actions are {0, 1}, so action space is length 2.
+        predicted_softmax_probabilities = self.forward(observations)
+
+        # Use the softmax probabilities to get the probabilities of each action.
+        # predicted_action_probabilities is shape [timesteps_per_batch, ]
+        predicted_action_probabilities = predicted_softmax_probabilities[torch.arange(num_examples), actions]
+
+        # Compute the reward.
+        advantages = advantages.type(predicted_action_probabilities.type())
+        J_value = torch.log(predicted_action_probabilities).dot(advantages) / num_examples
+        print(f'J_value: \t {J_value}') # Is this variable supposed to go down or up throughout training?
+
+        # Backprop.
+        J_value.backward()
+
+        # Update with the gradient step.
+        with torch.no_grad():
+            for param in self.parameters():
+                param.data += stepsize * param.grad
+        self.zero_grad()
 
     def learn(self, env):
         config = self.config
 
-        for iteration in range(config["n_iter"]):
+        for iteration in range(config["num_iterations"]):
 
             # Collect trajectories until we get timesteps_per_batch total timesteps.
             trajectories = []
@@ -116,40 +135,59 @@ class REINFORCEAgent(torch.nn.Module):
                 trajectories.append(trajectory)
                 total_timesteps += len(trajectory["reward"])
 
-            # Compute discounted sums of rewards.
+            # Each trajectory has observation, action, and reward.
+            # Observation is shape [length of trajectory, size of state space]
+            # Action is shape [length of trajectory, size of action space]
+            # Reward is shape [length of trajectory, ]
+
+            # After concatenating all observations, this is shape [timesteps_per_batch, size of state space]
+            observations = np.concatenate([trajectory["ob"] for trajectory in trajectories])
+
+            # After concatenating all actions, this is shape [timesteps_per_batch, size of action space].
+            actions = np.concatenate([trajectory["action"] for trajectory in trajectories])
+
+            # Compute returns, which is the discounted sums of rewards to-go.
+            # This is a list of returns. Each return is shape [length of trajectory, ].
             returns = [discount(trajectory["reward"], config["gamma"]) for trajectory in trajectories]
-            maxlen = max(len(_return) for _return in returns)
-            padded_returns = [np.concatenate([_return, np.zeros(maxlen-len(_return))]) for _return in returns]
 
             # Compute time-dependent baseline.
+            # baseline is a list of scalars, one for each time-step.
+            # The shape of baselines [max trajectory length, ]
+            max_trajectory_len = max(len(_return) for _return in returns)
+            padded_returns = [np.concatenate(
+                [_return, np.zeros(max_trajectory_len-len(_return))]) for _return in returns]
             baseline = np.mean(padded_returns, axis=0)
 
-            # Compute advantage function.
-            observations = np.concatenate([trajectory["ob"] for trajectory in trajectories])
-            actions = np.concatenate([trajectory["action"] for trajectory in trajectories])
+            # Subtract baseline to compute advantages.
+            # advantages is shape [timesteps_per_batch, ]
             advantages = [_return - baseline[:len(_return)] for _return in returns]
             advantages = np.concatenate(advantages)
-            # print(advantages[:10])
 
-            observations = torch.from_numpy(observations)
-            actions = torch.from_numpy(actions)
-            advantages = torch.from_numpy(advantages)
+            # Do the policy gradient step.
+            self.policy_gradient_update(
+                observations,
+                actions,
+                advantages,
+                config["stepsize"]
+            )
 
-            # Do the gradient step.
-            self.policy_gradient_update(observations, actions, advantages, config["stepsize"])
-
-            eprews = np.array([trajectory["reward"].sum() for trajectory in trajectories]) # episode total rewards
-            eplens = np.array([len(trajectory["reward"]) for trajectory in trajectories]) # episode lengths
+            # Log information.
+            episode_rewards = np.array([trajectory["reward"].sum() for trajectory in trajectories]) # episode total rewards
+            episode_lengths = np.array([len(trajectory["reward"]) for trajectory in trajectories]) # episode lengths
 
             print(f'------ Iteration {iteration} ------')
-            print(f'# Trajectories: {len(eprews)}')
-            print(f'# Timesteps:    {np.sum(eplens)}')
-            print(f'Max Reward:     {eprews.max():.1f}')
-            print(f'Mean Reward:    {eprews.mean():.1f} +- {eprews.std()/np.sqrt(len(eprews)):.1f}')
-            print(f'Mean Length:    {eplens.mean():.1f} +- {eplens.std()/np.sqrt(len(eplens)):.1f}')
+            print(f'# Trajectories: {len(episode_rewards)}')
+            print(f'# Timesteps:    {np.sum(episode_lengths)}')
+            print(f'Max reward:     {episode_rewards.max():.1f}')
+            print(f'Mean reward:    {episode_rewards.mean():.1f} std= {episode_rewards.std():.1f}')
+            print(f'Mean length:    {episode_lengths.mean():.1f} std= {episode_lengths.std():.1f}')
             print('\n\n')
-            get_trajectory(self, env, config["episode_max_length"], render=True)
+            # get_trajectory(self, env, config["episode_max_length"], render=True)
 
 environment = gym.make("CartPole-v1")
-agent = REINFORCEAgent(environment, episode_max_length=100, n_iter=1000, timesteps_per_batch=100000)
+# Actually run the experiment.
+agent = REINFORCEAgent(environment, episode_max_length=100, num_iterations=1000, timesteps_per_batch=100000)
+
+# Debug to print the shapes.
+# agent = REINFORCEAgent(environment, episode_max_length=30, num_iterations=1, timesteps_per_batch=150)
 agent.learn(environment)
